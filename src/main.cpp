1d2e5aa42280ecb3fcfa12a43aa0bc767ca262ca
Asad Waheed <asad.waheed.23@gmail.com>
2016-05-01 18:30:42 -0500

    coming back to this tomorrow, free() error not making sense right now

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,45 @@
 
 #include "ProcessSet.h"
 #include "Helpers.h"
+//#include "MemoryManager.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <pthread.h> // because you asked nicely...
+#include <unistd.h>
 
 using namespace std;
+
+static unsigned char our_memory[1024 * 1024]; //reserve 1 MB for malloc
+static size_t next_index = 0;
+
+void *my_malloc(size_t sz)
+{
+    void *mem;
+
+    if(sizeof our_memory - next_index < sz)
+        return NULL;
+
+    mem = &our_memory[next_index];
+    next_index += sz;
+    return mem;
+}
+
+void my_free(void *mem)
+{
+    if(mem)
+    {
+        free(mem);
+    } 
+    else 
+    {
+        cout << "\nInvalid function parameter!\n";
+    }
+}
+
+
+
 
 void sleepNanoSeconds(int sleepTime)
 {
@@ -80,6 +113,66 @@ int memorySimulatePartOne(ProcessSet process_set) {
     return 1;
 }
 
+// Part 2
+/*
+*   We simulate malloc and free using custom functions
+*   my_malloc and my_free to dynamically allocate and
+*   deallocate memory to each process. 
+*
+*   TODO: write the remaining part of the free function
+*   so far there is a trivial check there, but it needs to be
+*   be fixed. For future reference this is my bug at runtime:
+-------------------------------------------------------------
+Starting Memory Simulation Part Two
+Starting process 0
+*** Error in `./a.out': free(): invalid pointer: 0x00000000006073e0 ***
+--------------------------------------------------------------
+*   NOTE: Ignore all work done inside the MemoryManager class functions
+*   Abandoning OOP in favor of C-style functions
+*/
+int memorySimulatePartTwo(ProcessSet process_set) 
+{
+    
+    cout << "Starting Memory Simulation Part Two" << endl;
+    
+    
+    auto totalTimeStart = chrono::high_resolution_clock::now();
+    
+    void *allocatedMemory = NULL;
+    
+    // Run all 64 processes
+    while(process_set.GetNumberProcesses() > 0) 
+    {
+        auto memAllocStart = chrono::high_resolution_clock::now();
+        
+        Process currentProcess = process_set.FirstProcess();
+        
+        cout << "Starting process " << currentProcess.processId << endl;
+        
+        int memoryFootPrintForCurrentProcess = currentProcess.memoryFootprint;
+        
+        // Allocate memory for this process
+        allocatedMemory = my_malloc(memoryFootPrintForCurrentProcess * 1024); // The memory footprint is in KB
+        
+        if (allocatedMemory == NULL) 
+        {
+            cout << "Error allocating memory for process with process ID " << currentProcess.processId << endl ;
+            return -2;
+        }
+        // free the memory for this process
+        my_free(allocatedMemory);
+        
+        // Delete the process since we are done.
+        process_set.PopProcess();
+
+    }
+    
+    auto totalTimeEnd = chrono::high_resolution_clock::now();
+    cout << "Total program execution time: " << (chrono::duration_cast<chrono::nanoseconds>(totalTimeEnd - totalTimeStart).count()) << " nanoseconds" << endl;
+    
+    return 1;
+}
+
 int main(int argc, char** argv)
 {
     // Create a set of processes
@@ -88,6 +181,7 @@ int main(int argc, char** argv)
     
     // Run Part one
     memorySimulatePartOne(process_set);
-    
+    // Run Part 2
+    memorySimulatePartTwo(process_set);
 	return 0;
 }
